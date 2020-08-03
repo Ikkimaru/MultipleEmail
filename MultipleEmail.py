04d47249c471b86471bc11f1db71ac3b43ec1210
Ikkimaru <ebenvrensburg@hotmail.com>
2020-08-03 16:41:03 -0400

    Corrected emails not sending. Added Function to select Template. Improved Warning Message

--- MultipleEmail.py
+++ MultipleEmail.py
@@ -2,6 +2,7 @@
 # https://www.freecodecamp.org/news/send-emails-using-code-4fcea9df63f/
 # https://blog.mailtrap.io/sending-emails-in-python-tutorial-with-code-examples/
 
+import sys
 import os 		#File Directory
 import smtplib	#SMTP Server
 
@@ -21,6 +22,7 @@ PASSWORD = 'Password123'						#Sender Password
 UNIVERSAL_DOMAIN = '@domain.ac.za' 				#Receiver emails
 FILE_LOCATION = 'PDF_Files/'				
 MESSAGE_TEMPLATE_LOCATION = 'message.txt'
+TXT_CONTAINS_VARIABLE = False
 MESSAGE_SUBJECT = "This is a test"
 #####################################################################
 
@@ -34,57 +36,64 @@ def get_emails_from_files(directory):
 def read_message(filename): #Create email message form txt template
 	with open(filename, 'r', encoding='utf-8') as template_file:
 		template_file_content = template_file.read()
-	return Template(template_file_content)
+	if(TXT_CONTAINS_VARIABLE):
+		return Template(template_file_content)
+	else:
+		return template_file_content
+
+response = input("Universal domain: "+ UNIVERSAL_DOMAIN + "\nPdf file Location: " + FILE_LOCATION + "\nMessage Template: "+ MESSAGE_TEMPLATE_LOCATION + "\nThe message file contain a variable: " + str(TXT_CONTAINS_VARIABLE) + "\nMessage Subject: " + MESSAGE_SUBJECT+"\n\nAre these values correct? (Y/N) ")
+if response == "Y" or response == "y":
+	def main():
+		emails = get_emails_from_files(FILE_LOCATION)				#Get Emails
+		message_template = read_message(MESSAGE_TEMPLATE_LOCATION)	#Create Message
+
+		#Set up the SMTP server
+		s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
+		s.starttls()
+		s.login(MY_ADDRESS,PASSWORD)
+
+		for name in emails:
+			msg = MIMEMultipart() 		#create a message
+
+			if(TXT_CONTAINS_VARIABLE):
+				message = message_template.substitute(PERSON_NAME=name)		#Sustitute Name Variable from txt file ------${PERSON_NAME}
+			else:
+				message = message_template
+
+			#print(message) 				#For testing purposes
+			print('Sending to '+name)		#For Progress Report
+
+			email = name + UNIVERSAL_DOMAIN		#Create email address from file name
+
+			#Setup for the email parameters
+			msg['From']=MY_ADDRESS
+			msg['To']=email
+			msg['Subject']=MESSAGE_SUBJECT
+
+			#Add Pdf File
+			filename = FILE_LOCATION+name+'.pdf'
+			with open(filename, "rb") as attachment:
+				part = MIMEBase("application","octet-stream")
+				part.set_payload(attachment.read())
+
+			encoders.encode_base64(part)
+			part.add_header('Content-Disposition','attachment', filename=name+'.pdf')
+			msg.attach(part)
 
 
-def main():
-	emails = get_emails_from_files(FILE_LOCATION)				#Get Emails
-	message_template = read_message(MESSAGE_TEMPLATE_LOCATION)	#Create Message
+			#Add txt message
+			msg.attach(MIMEText(message, 'plain'))
 
-	#Set up the SMTP server
-	s = smtplib.SMTP(host='smtp-mail.outlook.com', port=587)
-	s.starttls()
-	s.login(MY_ADDRESS,PASSWORD)
+			#s.sendmail(msg)
+			s.sendmail(MY_ADDRESS, [email], msg.as_string())
+			print('Sent to ' + name)
+			del msg
 
-	for name in emails:
-		msg = MIMEMultipart() 		#create a message
-
-		#message = message_template.substitute(PERSON_NAME=name.title())		#Sustitute Name Variable from txt file ------${PERSON_NAME}
-
-		#print(message) 				#For testing purposes
-		print('Sending to '+name)		#For Progress Report
-
-		email = name + UNIVERSAL_DOMAIN		#Create email address from file name
-
-		#Setup for the email parameters
-		msg['From']=MY_ADDRESS
-		msg['To']=email
-		msg['Subject']=MESSAGE_SUBJECT
-
-		#Add Pdf File
-		filename = FILE_LOCATION+name+'.pdf'
-		with open(filename, "rb") as attachment:
-			part = MIMEBase("application","octet-stream")
-			part.set_payload(attachment.read())
-
-		encoders.encode_base64(part)
-		part.add_header('Content-Disposition','attachment', filename=name+'.pdf')
-		msg.attach(part)
-
-
-		#Add txt message
-		msg.attach(MIMEText(message, 'plain'))
-
-		#s.sendmail(msg)
-		s.sendmail(MY_ADDRESS, [email], msg.as_string())
-		print('Sent to ' + name)
-		del msg
-
-	#Terminate SMTP session
-	s.quit()
+		#Terminate SMTP session
+		s.quit()
+else:
+	sys.exit()
 
 
 if __name__ == '__main__':
-	response = input("Did you check the variables? (Y/N) ")
-	if response == "Y" or response == "y":
-		main()
+	main()
